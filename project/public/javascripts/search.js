fb0dfc6e8b70505d3df9c130c5ef762f078e0b0a
Han_Han <2088113262@qq.com>
2023-07-12 12:09:28 +0800

    project

--- project/public/javascripts/search.js
+++ project/public/javascripts/search.js
@@ -7,23 +7,25 @@ function showResults() {
     var resultBody = document.getElementById('tbody');
     var prevButton = document.getElementById('prev-btn');
     var nextButton = document.getElementById('next-btn');
+    var paginationContainer = document.getElementById('pagination');
     var totalPages = Math.ceil(data.length / resultsPerPage);
 
     resultBody.innerHTML = '';
     resultContainer.style.display = "block";
+    paginationContainer.innerHTML = ''; // 清空分页索引容器
 
     var startIndex = (currentPage - 1) * resultsPerPage;
     var endIndex = startIndex + resultsPerPage;
 
-    var id = 1;
+    // var id = 1;
     for (let i = startIndex; i < endIndex && i < data.length; i++) {
         var news = data[i];
         var row = document.createElement('tr');
-        if (id%2 === 0){
+        if ((i+1)%2 === 0){
             row.className = 'ss';
         }
         row.innerHTML = `
-                        <td>${id}</td>
+                        <td>${i+1}</td>
                         <td>${news.title}</td>
                         <td>${news.author}</td>
                         <td>${news.source_name}</td>
@@ -32,12 +34,36 @@ function showResults() {
                         <td>${news.publish_date}</td>
                       `;
         resultBody.appendChild(row);
-        id += 1;
+        // id += 1;
     }
 
     prevButton.disabled = currentPage === 1;
     nextButton.disabled = currentPage === totalPages;
 
+    // 添加分页索引按钮
+    for (let i = 1; i <= totalPages; i++) {
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.innerText = i;
+        a.id = i;
+        a.onclick = function() {
+            currentPage = i;
+            showResults();
+        };
+
+        if (currentPage === i) {
+            a.style.color = "black"; // 设置当前页按钮的颜色
+            // a.style::after(function (){
+            //     content: "";
+            //     display: 'block';
+            //     background: 'linear-gradient(to right, #188eee, #999)';
+            //     width: '100%';
+            //     height: '2px';
+            // })
+        }
+        li.appendChild(a);
+        paginationContainer.appendChild(li);
+    }
 }
 
 function goToPrevPage() {
