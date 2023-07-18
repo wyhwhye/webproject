0e650170d941e64904cb58f99b09808ef37aa68e
Han_Han <2088113262@qq.com>
2023-07-18 21:13:20 +0800

    封装

--- project/public/javascripts/search.js
+++ project/public/javascripts/search.js
@@ -50,8 +50,85 @@ function showResults() {
 
     /* 添加分页按钮 */
     if (totalPages <= 7){  // 如果 totalPages <= 7页，全部显示
-        // 创建  <li><a></a></li> 标签作为按钮
-        for (let i = 1; i <= totalPages; i++) {
+        addSuccessivePageButton(1, totalPages);
+    }
+    else if (totalPages > 7){  // 如果 totalPages > 7页，部分显示
+        // console.log(currentPage);
+        /* 根据当前页决定分页显示格式，“...”相应为中间页 */
+        switch (currentPage){
+            case 1:  // < 1 2 3 4 5 ... c >
+            case 2:  // < 1 2 3 4 5 ... c >
+            case 3:  // < 1 2 3 4 5 ... c >
+            case 4:  // < 1 2 3 4 5 ... c >
+                addSuccessivePageButton(1, 5);
+                addDotsPageButton(Math.ceil((5+totalPages)/2));
+                addLastPageButton();
+                break;
+
+            case totalPages-3:  // < 1 ... c-4 c-3 c-1 c-1 c >
+            case totalPages-2:  // < 1 ... c-4 c-3 c-1 c-1 c >
+            case totalPages-1:  // < 1 ... c-4 c-3 c-1 c-1 c >
+            case totalPages:    // < 1 ... c-4 c-3 c-1 c-1 c >
+                addFirstPageButton();
+                addDotsPageButton(Math.ceil((1 + totalPages-4)/2));
+                addSuccessivePageButton(totalPages-4, totalPages);
+                break;
+
+            // < 1 ... x-1 x x+1 ... c >
+            default:
+                addFirstPageButton();
+                addDotsPageButton(Math.ceil((1 + currentPage-1)/2));
+                addSuccessivePageButton(currentPage-1, currentPage+1);
+                addDotsPageButton(Math.ceil((currentPage+1 + totalPages)/2));
+                addLastPageButton();
+        }
+    }
+
+    // 首页
+    function addFirstPageButton() {
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.innerText = 1;
+        a.id = 1;
+        a.onclick = function() {
+            currentPage = 1;
+            showResults();
+        };
+        li.appendChild(a);
+        paginationContainer.appendChild(li);
+    }
+
+    // 末页
+    function addLastPageButton() {
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.innerText = totalPages;
+        a.id = totalPages;
+        a.onclick = function() {
+            currentPage = totalPages;
+            showResults();
+        };
+        li.appendChild(a);
+        paginationContainer.appendChild(li);
+    }
+
+    // ...
+    function addDotsPageButton(page) {
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.innerText = '...';
+        a.id = '...';
+        a.onclick = function() {
+            currentPage = page;
+            showResults();
+        };
+        li.appendChild(a);
+        paginationContainer.appendChild(li);
+    }
+
+    // 连续页
+    function addSuccessivePageButton(start, end) {
+        for (let i = start; i <= end; i++) {
             var li = document.createElement('li');
             var a = document.createElement('a');
             a.innerText = i;
@@ -68,172 +145,7 @@ function showResults() {
             paginationContainer.appendChild(li);
         }
     }
-    else if (totalPages > 7){  // 如果 totalPages > 7页，部分显示
-        // console.log(currentPage);
-        /* 根据当前页决定分页显示格式，“...”相应为中间页 */
-        switch (currentPage){
-            case 1:  // < 1 2 3 4 5 ... c >
-            case 2:  // < 1 2 3 4 5 ... c >
-            case 3:  // < 1 2 3 4 5 ... c >
-            case 4:  // < 1 2 3 4 5 ... c >
-                for (let i = 1; i <= 5; i++) {
-                    var li = document.createElement('li');
-                    var a = document.createElement('a');
-                    a.innerText = i;
-                    a.id = i;
-                    a.onclick = function() {
-                        currentPage = i;
-                        showResults();
-                    };
-                    if (currentPage === i) {
-                        a.style.color = "black";
-                        a.classList.add('show-after');
-                    }
-                    li.appendChild(a);
-                    paginationContainer.appendChild(li);
-                }
 
-                // ...
-                var li = document.createElement('li');
-                var a = document.createElement('a');
-                a.innerText = '...';
-                a.id = '...';
-                a.onclick = function() {
-                    currentPage = Math.ceil((5+totalPages)/2);
-                    showResults();
-                };
-                li.appendChild(a);
-                paginationContainer.appendChild(li);
-
-                // 末页
-                var li = document.createElement('li');
-                var a = document.createElement('a');
-                a.innerText = totalPages;
-                a.id = totalPages;
-                a.onclick = function() {
-                    currentPage = totalPages;
-                    showResults();
-                };
-                li.appendChild(a);
-                paginationContainer.appendChild(li);
-                break;
-
-            case totalPages-3:  // < 1 ... c-4 c-3 c-1 c-1 c >
-            case totalPages-2:  // < 1 ... c-4 c-3 c-1 c-1 c >
-            case totalPages-1:  // < 1 ... c-4 c-3 c-1 c-1 c >
-            case totalPages:    // < 1 ... c-4 c-3 c-1 c-1 c >
-
-                // 首页
-                var li = document.createElement('li');
-                var a = document.createElement('a');
-                a.innerText = 1;
-                a.id = 1;
-                a.onclick = function() {
-                    currentPage = 1;
-                    showResults();
-                };
-                li.appendChild(a);
-                paginationContainer.appendChild(li);
-
-                // ...
-                var li = document.createElement('li');
-                var a = document.createElement('a');
-                a.innerText = '...';
-                a.id = '...';
-                a.onclick = function() {
-                    currentPage = Math.ceil((1 + totalPages-4)/2);
-                    showResults();
-                };
-                li.appendChild(a);
-                paginationContainer.appendChild(li);
-
-                for (let i = totalPages-4; i <= totalPages; i++) {
-                    var li = document.createElement('li');
-                    var a = document.createElement('a');
-                    a.innerText = i;
-                    a.id = i;
-                    a.onclick = function() {
-                        currentPage = i;
-                        showResults();
-                    };
-                    if (currentPage === i) {
-                        a.style.color = "black";
-                        a.classList.add('show-after');
-                    }
-                    li.appendChild(a);
-                    paginationContainer.appendChild(li);
-                }
-                break;
-
-            // < 1 ... x-1 x x+1 ... c >
-            default:
-                // 首页
-                var li = document.createElement('li');
-                var a = document.createElement('a');
-                a.innerText = 1;
-                a.id = 1;
-                a.onclick = function() {
-                    currentPage = 1;
-                    showResults();
-                };
-                li.appendChild(a);
-                paginationContainer.appendChild(li);
-
-                // ...
-                var li = document.createElement('li');
-                var a = document.createElement('a');
-                a.innerText = '...';
-                a.id = '...';
-                a.onclick = function() {
-                    currentPage = Math.ceil((1 + currentPage-1)/2);
-                    showResults();
-                };
-                li.appendChild(a);
-                paginationContainer.appendChild(li);
-
-                for (let i = currentPage-1; i <= currentPage+1; i++) {
-                    var li = document.createElement('li');
-                    var a = document.createElement('a');
-                    a.innerText = i;
-                    a.id = i;
-                    a.onclick = function() {
-                        currentPage = i;
-                        showResults();
-                    };
-                    if (currentPage === i) {
-                        a.style.color = "black";
-                        a.classList.add('show-after');
-                    }
-                    li.appendChild(a);
-                    paginationContainer.appendChild(li);
-                }
-
-                // ...
-                var li = document.createElement('li');
-                var a = document.createElement('a');
-                a.innerText = '...';
-                a.id = '...';
-                a.onclick = function() {
-                    currentPage = Math.ceil((currentPage+1 + totalPages)/2);
-                    showResults();
-                };
-                li.appendChild(a);
-                paginationContainer.appendChild(li);
-
-                // 末页
-                var li = document.createElement('li');
-                var a = document.createElement('a');
-                a.innerText = totalPages;
-                a.id = totalPages;
-                a.onclick = function() {
-                    currentPage = totalPages;
-                    showResults();
-                };
-                li.appendChild(a);
-                paginationContainer.appendChild(li);
-
-        }
-    }
 }
 
 function goToPrevPage() {
